37b74937cc40e11416d97956c4f7ef754722763b
ismail harik <92827404+Ismailharik@users.noreply.github.com>
2023-02-28 14:56:14 +0100

    fix some styles

--- src/app/shop/components/related-cars/related-cars.component.ts
+++ src/app/shop/components/related-cars/related-cars.component.ts
@@ -36,20 +36,6 @@ export class RelatedCarsComponent implements OnInit {
       '<i class="fa fa-angle-left" aria-hidden="true"></i>',
       '<i class="fa fa-angle-right" aria-hidden="true"></i>'
     ],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 2
-      },
-      740: {
-        items: 3
-      },
-      940: {
-        items: 4
-      }
-    },
   }
 
   getVehiclesByCategory(){
